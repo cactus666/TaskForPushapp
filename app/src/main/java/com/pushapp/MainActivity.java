e4943e784dbd41900ada100df1fe946052a43898
TechnoC <vt_shustiv@mail.ru>
2019-06-12 02:24:11 +0400

    много всего...(

--- app/src/main/java/com/pushapp/MainActivity.java
+++ app/src/main/java/com/pushapp/MainActivity.java
@@ -4,12 +4,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
+
+import com.pushapp.POJO.Password;
 
 import net.sqlcipher.database.SQLiteDatabase;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView mRecyclerView;
+    private List<Password> passwords = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +27,13 @@ public class MainActivity extends AppCompatActivity {
 
         SQLiteDatabase.loadLibs(this);
 
-
         getSupportActionBar().setTitle(R.string.title_first_act);
 
-
+        passwords.add(new Password("lol", "1w", "er", "12w", 1));
+        mRecyclerView = findViewById(R.id.recycler_view_passwords);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setAdapter(new PassAdapter(passwords));
 
 
         FloatingActionButton fab = findViewById(R.id.fab);
@@ -34,8 +46,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
-
 
 
 }
--- app/src/main/java/com/pushapp/NewRecordActivity.java
+++ app/src/main/java/com/pushapp/NewRecordActivity.java
@@ -2,56 +2,109 @@ package com.pushapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.text.method.PasswordTransformationMethod;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import static com.pushapp.DB.DBHelper.getInstance;
 
 public class NewRecordActivity extends AppCompatActivity {
+
+    private Button btn_save;
+    private EditText name;
+    private EditText link;
+    private EditText login;
+    private EditText val_pass;
+    private EditText val_pass2;
+    private ImageButton icon_pass;
+    private ImageButton icon_pass2;
+    private ImageView arrow_back;
+
+    View.OnClickListener listener_for_pass_icon = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if(v.getId() == icon_pass2.getId())
+                if (val_pass2.getTransformationMethod() == null)
+                    val_pass2.setTransformationMethod(new PasswordTransformationMethod());
+                else
+                    val_pass2.setTransformationMethod(null);
+            else
+                if (val_pass.getTransformationMethod() == null)
+                    val_pass.setTransformationMethod(new PasswordTransformationMethod());
+                else
+                    val_pass.setTransformationMethod(null);
+        }
+    };
+
+    View.OnClickListener listener_save_btn = new View.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            if( (val_pass.getText().toString().intern() == val_pass2.getText().toString().intern() ) && (val_pass.getText().toString().trim().length() != 0) ){
+                // еще проверка на наличие пароля
+                getInstance(NewRecordActivity.this).insertNewPass(val_pass.getText().toString());
+            }else{
+                Toast.makeText(NewRecordActivity.this, "fail", Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
+
+    View.OnClickListener listener_arrow_back = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            finish();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_record);
 
-       // Toolbar toolbar = findViewById(R.id.toolbar);
-       // setSupportActionBar(toolbar);
+        btn_save = (Button)findViewById(R.id.save_btn);
+        name = (EditText)findViewById(R.id.name);
+        link = (EditText)findViewById(R.id.link);
+        login = (EditText)findViewById(R.id.login);
+        val_pass = (EditText)findViewById(R.id.val_pass);
+        val_pass2 = (EditText)findViewById(R.id.val_pass2);
+        icon_pass = findViewById(R.id.pass);
+        icon_pass2 = findViewById(R.id.pass2);
+        arrow_back = findViewById(R.id.back_arrow);
 
-        final EditText val_pass2_et = (EditText)findViewById(R.id.val_pass2);
+        icon_pass.setOnClickListener(listener_for_pass_icon);
+        icon_pass2.setOnClickListener(listener_for_pass_icon);
+        btn_save.setOnClickListener(listener_save_btn);
+        arrow_back.setOnClickListener(listener_arrow_back);
 
-        findViewById(R.id.pass2).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (val_pass2_et.getTransformationMethod() == null)
-                    val_pass2_et.setTransformationMethod(new PasswordTransformationMethod());
-                else
-                    val_pass2_et.setTransformationMethod(null);
-            }
-        });
+         Toolbar toolbar = findViewById(R.id.toolbar);
+         setSupportActionBar(toolbar);
 
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 }
